45ab420db447da176d3023d04d1aac0a5b9466fb
likui628 <likui.it@gmail.com>
2021-12-19 20:56:25 +0800

    login validating

--- vue3-forms/src/views/LoginForm.vue
+++ vue3-forms/src/views/LoginForm.vue
@@ -2,10 +2,21 @@
     <div>
         <form @submit.prevent="onSubmit">
             <fieldset>
-                <BaseInput label="Email" type="email" v-model="email" />
+                <BaseInput
+                    label="Email"
+                    type="text"
+                    :modelValue="email"
+                    @change="handleChange"
+                    :error="errors.email"
+                />
             </fieldset>
             <fieldset>
-                <BaseInput label="Password" type="password" v-model="password" />
+                <BaseInput
+                    label="Password"
+                    type="text"
+                    v-model="password"
+                    :error="errors.password"
+                />
             </fieldset>
             <BaseButton type="submit" class="-fill-gradient">Submit</BaseButton>
         </form>
@@ -13,20 +24,31 @@
 </template>
 
 <script>
+import { useField, useForm } from 'vee-validate'
+import { object as yupObject, string } from 'yup'
 
 export default {
-  data () {
-    return {
-      email: '',
-      password: ''
-    }
-  },
   setup () {
-    function onSubmit () {
-      alert('Submitted')
-    }
+    const schema = yupObject({
+      email: string().required().email(),
+      password: string().required().min(8)
+    })
+
+    const { handleSubmit, errors } = useForm({
+      validationSchema: schema
+    })
+    const { value: email, handleChange } = useField('email')
+    const { value: password } = useField('password')
+
+    const onSubmit = handleSubmit(values => {
+      console.log('submit', values)
+    })
 
     return {
+      email,
+      handleChange,
+      password,
+      errors,
       onSubmit
     }
   }
